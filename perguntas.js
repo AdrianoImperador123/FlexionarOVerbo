6b74fb5ced56ff9c560e29ec5dbfd7241d97b557
ProgramadorIniciante <00001090526398sp@al.educacao.sp.gov.br>
2024-10-29 09:35:09 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2=?',
+    'A resposta é 4]'
+)
+
+criaCartao(
+    'Minecraft',
+    'Como é o nome do mob preto? Ele só atacara o jogador, caso olhe diretamente nos olhos dele',
+    'Enderman, se você disse outra coisa, procure ajuda!'
+)
+
+criaCartao(
+    'Anime',
+    'Qual é o protagosnista do anime "Naruto"?',
+    'Naruto.'
+)
+
+criaCartao(
+    'Geografia',
+    'Por que o céu é azul?',
+    'O céu é azul porque a luz solar, ao atravessar a atmosfera terrestre, é refratada e atinge os átomos dos gases oxigênio e nitrogênio, que a dispersa em todas as suas cores, com seus respectivos comprimentos de onda, refletidos por essas partículas. A cor que se dispersa e é mais bem refletida é a azul.'
+)
